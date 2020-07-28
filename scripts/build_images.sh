4f183f0d305aec78b1172f3b84f46179dc280bb5
coolwill92 <willpearce101@gmail.com>
2020-07-28 11:01:24 +0000

    jenkinsfile4

--- scripts/build_images.sh
+++ scripts/build_images.sh
@@ -6,18 +6,14 @@ if [[ "$(docker images -q coolwill92/service_1:latest 2> /dev/null)" == "" ]]; t
     docker build -t coolwill92/service_1 ./Service_1
 fi
 
-
 if [[ "$(docker images -q coolwill92/service_2:latest 2> /dev/null)" == "" ]]; then
     docker build -t coolwill92/service_2 ./Service_2
 fi
 
-
-if [[ "$(docker images -q coolwill92/service_3:latest 3> /dev/null)" == "" ]]; then
+if [[ "$(docker images -q coolwill92/service_3:latest 2> /dev/null)" == "" ]]; then
     docker build -t coolwill92/service_3 ./Service_3
 fi
-
 
 if [[ "$(docker images -q coolwill92/service_4:latest 2> /dev/null)" == "" ]]; then
     docker build -t coolwill92/service_4 ./Service_4
 fi
-
