b0cf5d695be2ca19ae48deb4be306750fcbc40c1
coolwill92 <willpearce101@gmail.com>
2020-07-28 10:22:25 +0000

    building images script

--- Jenkinsfile
+++ Jenkinsfile
@@ -8,12 +8,9 @@ pipeline{
             
             steps {
 
-                sh 'docker build -t coolwill92/service_1 ./Service_1'
-                sh 'docker build -t coolwill92/service_2 ./Service_2'
-                sh 'docker build -t coolwill92/service_3 ./Service_3'
-                sh 'docker build -t coolwill92/service_4 ./Service_4'
+'
                 sh 'chmod +x ./scripts/*.sh'
-                sh './scripts/before_installation.sh'
+                sh './scripts/build_images.sh'
 
             }
             
---
+++ scripts/build_images.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Bulding service images if it does not exist locally
+
+if [[ "$(docker images -q coolwill92/service_1:latest 2> /dev/null)" == "" ]]; then
+    docker build -t coolwill92/service_1 ../Service_1
+fi
+
+if [[ "$(docker images -q coolwill92/service_2:latest 2> /dev/null)" == "" ]]; then
+    docker build -t coolwill92/service_2 ../Service_2
+fi
+
+if [[ "$(docker images -q coolwill92/service_3:latest 2> /dev/null)" == "" ]]; then
+    docker build -t coolwill92/service_3 ../Service_3
+fi
+
+if [[ "$(docker images -q coolwill92/service_4:latest 2> /dev/null)" == "" ]]; then
+    docker build -t coolwill92/service_4 ../Service_4
+fi
