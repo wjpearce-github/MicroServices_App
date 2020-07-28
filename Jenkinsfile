003866af381d55306616a04c62249fbe0ac9c423
coolwill92 <willpearce101@gmail.com>
2020-07-28 15:40:43 +0000

    Testing changes to index to see if they show on final build 10

--- Jenkinsfile
+++ Jenkinsfile
@@ -10,7 +10,7 @@ pipeline{
 
                 sh 'chmod +x ./scripts/*.sh'
                 sh './scripts/build_images.sh'
-                sh './scripts/pull images.sh'
+                sh './scripts/pull_images.sh'
                 sh './scripts/build_swarm.sh'
                 sh './scripts/update_swarm.sh'
             }
--- scripts/pull_images.sh
+++ scripts/pull_images.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 
-#docker pull coolwill92/service_1
-#docker pull coolwill92/service_2
-#docker pull coolwill92/service_3
-#docker pull coolwill92/service_4
+docker pull coolwill92/service_1
+docker pull coolwill92/service_2
+docker pull coolwill92/service_3
+docker pull coolwill92/service_4
 
-# if service exists then docker service update --image coolwill92/services_1 secondapp_service_1
+# if service exists then docker service update --image 
+# coolwill92/services_1 secondapp_service_1
