c516eabfd799633fc05e4d6d43342f8dc25adc4f
coolwill92 <willpearce101@gmail.com>
2020-07-28 15:37:02 +0000

    Testing changes to index to see if they show on final build 8

--- scripts/pull_images.sh
+++ scripts/pull_images.sh
@@ -5,3 +5,5 @@
 #docker pull coolwill92/service_2
 #docker pull coolwill92/service_3
 #docker pull coolwill92/service_4
+
+# if service exists then docker service update --image coolwill92/services_1 secondapp_service_1
