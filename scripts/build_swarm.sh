a7b92768a50d5fb88a754ebf7577a95890f7e993
coolwill92 <willpearce101@gmail.com>
2020-07-28 11:56:07 +0000

    Testing Swarm Script2

--- scripts/build_swarm.sh
+++ scripts/build_swarm.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#Takes running services and builds swarm 
+#Takes running services and builds swarm! 
 
 if [[ "$(docker stack services secondapp 2> /dev/null)" == "" ]]; then
     docker stack deploy --compose-file docker-compose.yml secondapp
