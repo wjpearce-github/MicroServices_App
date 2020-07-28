eb27ced33489079d45dd8bd54dea248ab6823293
coolwill92 <willpearce101@gmail.com>
2020-07-28 10:30:34 +0000

    building images script3

--- scripts/build_images.sh
+++ scripts/build_images.sh
@@ -2,22 +2,22 @@
 
 #Sript to build images if non exist locally
 
-if [["$(docker images -q coolwill92/service_1:latest 2> /dev/null)" == ""]]; then
-    docker build -t coolwill92/service_1 ../Service_1
+if [[ "$(docker images -q coolwill92/service_1:latest 2> /dev/null)" == "" ]]; then
+    docker build -t coolwill92/service_1 ./Service_1
 fi
 
 
-if [["$(docker images -q coolwill92/service_2:latest 2> /dev/null)" == ""]]; then
-    docker build -t coolwill92/service_2 ../Service_2
+if [[ "$(docker images -q coolwill92/service_2:latest 2> /dev/null)" == "" ]]; then
+    docker build -t coolwill92/service_2 ./Service_2
 fi
 
 
-if [["$(docker images -q coolwill92/service_1:latest 3> /dev/null)" == ""]]; then
-    docker build -t coolwill92/service_3 ../Service_3
+if [[ "$(docker images -q coolwill92/service_1:latest 3> /dev/null)" == "" ]]; then
+    docker build -t coolwill92/service_3 ./Service_3
 fi
 
 
-if [["$(docker images -q coolwill92/service_4:latest 2> /dev/null)" == ""]]; then
-    docker build -t coolwill92/service_4 ../Service_4
+if [[ "$(docker images -q coolwill92/service_4:latest 2> /dev/null)" == "" ]]; then
+    docker build -t coolwill92/service_4 ./Service_4
 fi
 
