7d7711e9582a2bba9fd3734c0e19ede50d4ec7fa
coolwill92 <willpearce101@gmail.com>
2020-07-29 09:55:47 +0000

    Testing changes

--- scripts/update_swarm.sh
+++ scripts/update_swarm.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 docker service update --image coolwill92/service_1:latest secondapp_service_1
+
 docker service update --image coolwill92/service_2:latest secondapp_service_2
+
 docker service update --image coolwill92/service_3:latest secondapp_service_3
+
 docker service update --image coolwill92/service_4:latest secondapp_service_4
