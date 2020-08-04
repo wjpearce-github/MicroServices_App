5439d5f0f231f7d31793d30b32fcac6db9566160
coolwill92 <willpearce101@gmail.com>
2020-08-04 09:52:41 +0000

    Testing7

--- Service_1/application/routes.py
+++ Service_1/application/routes.py
@@ -5,41 +5,11 @@ from os import environ
 import requests
 import random
 
-app.config['SECRET_KEY'] = '60ae1c92bc03176e8976331683eb9c54' 
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://' + \
-                                        environ.get('MYSQL_USER') + \
-                                        ':' + \
-                                        environ.get('MYSQL_PASSWORD') + \
-                                        '@' + \
-                                        environ.get('MYSQL_HOST') + \
-                                        ':' + \
-                                        environ.get('MYSQL_PORT') + \
-                                        '/' + \
-                                        environ.get('MYSQL_DB_NAME')
-
-db = SQLAlchemy(app)
-
-class games(db.Model):
-    id = db.Column(db.Integer, primary_key=True, unique=True)
-    f_name = db.Column(db.String(50), nullable=False)
-    l_name = db.Column(db.String(50), nullable=False)
-   
-    def __repr__(self):
-        return ''.join(
-        [
-            'First name ' + self.f_name + '\n' 
-            'Lat name ' + self.l_name + '\n'
-            'ID: ' + str(self.id)
-        ]
-    )
 
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/', methods=['GET'])
 def home():
-    response = requests.get('http://service_4:5003/merge')
-    print(response)
-    sentence = response.text
-    dnd_data = dnd.query.all()
-    return render_template('index.html', sentence=sentence, dnd=dnd_data, title='Home')
+    response = requests.get('http://service_4:5003/rpgname')
+    rpgname = response.text
+    return render_template('index.html', rpgname = rpgname, title = 'Home')
 
--- scripts/build_swarm.sh
+++ scripts/build_swarm.sh
@@ -2,8 +2,6 @@
 
 #Takes running services and builds swarm! 
 
-source /var/lib/jenkins/.env
-
 if [[ "$(docker stack services secondapp 2> /dev/null)" == "" ]]; then
     docker stack deploy --compose-file docker-compose.yml secondapp
 fi
