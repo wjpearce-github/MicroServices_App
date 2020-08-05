a08719186da4d6a49da3788ed157e83f4533eee3
coolwill92 <willpearce101@gmail.com>
2020-08-05 10:03:19 +0000

    test

--- Service_1/application/routes.py
+++ Service_1/application/routes.py
@@ -3,23 +3,20 @@ from flask import render_template, request
 from flask_sqlalchemy import SQLAlchemy
 from os import environ
 import requests
-import os
 import random
-
 
 app.config['SECRET_KEY'] = environ.get('SECRET_KEY')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://' + \
-                                        environ.get('MYSQL_USER') + \
-                                        ':' + \
-                                        environ.get('MYSQL_PASSWORD') + \
-                                        '@' + \
-                                        environ.get('MYSQL_HOST') + \
-                                        ':' + \
-                                        environ.get('MYSQL_PORT') + \
-                                        '/' + \
-                                        environ.get('MYSQL_DB_NAME')
-
+                                         environ.get('MYSQL_USER') + \
+                                         ':' + \
+                                         environ.get('MYSQL_PASSWORD') + \
+                                         '@' + \
+                                         environ.get('MYSQL_HOST') + \
+                                         ':' + \
+                                         environ.get('MYSQL_PORT') + \
+                                         '/' + \
+                                         environ.get('MYSQL_DB_NAME')
 
 db = SQLAlchemy(app)
 
@@ -27,20 +24,19 @@ class games(db.Model):
     id = db.Column(db.Integer, primary_key=True, unique=True)
     f_name = db.Column(db.String(50), nullable=False)
 
+
    
     def __repr__(self):
         return ''.join(
         [
-            'First name: ' + self.f_name + '\n'  
+            'First name ' + self.f_name + '\n'  
             'ID: ' + str(self.id)
         ]
     )
 
-
-
 @app.route('/', methods=['GET'])
 def home():
-    response = requests.get('http://service_4:5003/randomword')
+    response = requests.get('http://Service_4:5003/randomword')
     print(response)
     game = response.text
     game_data = games(
