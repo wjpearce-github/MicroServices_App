dcaec57091bb26bb134f1c1458064671cc9053a8
coolwill92 <willpearce101@gmail.com>
2020-08-05 15:52:15 +0000

    jenkins test

--- scripts/update_swarm.sh
+++ scripts/update_swarm.sh
@@ -3,11 +3,15 @@
 #updates images
 
 docker service update --image coolwill92/service_1:latest secondapp_service_1
+sleep 5
 
 docker service update --image coolwill92/service_2:latest secondapp_service_2
+sleep 5
 
 docker service update --image coolwill92/service_3:latest secondapp_service_3
+sleep 5
 
 docker service update --image coolwill92/service_4:latest secondapp_service_4
+sleep 5
 
 docker system prune -f
