4e5ac633df81737cd227293e4557acfe6cf848af
coolwill92 <willpearce101@gmail.com>
2020-08-05 14:42:33 +0000

    tsaf

--- scripts/build_images.sh
+++ scripts/build_images.sh
@@ -2,19 +2,21 @@
 
 #Sript to build images if non exist locally to save time!
 
-docker build -t coolwill92/service_1 ./Service_1
+
+
+docker build --no-cache -t coolwill92/service_1 ./Service_1
 docker push coolwill92/service_1
 
-docker build -t coolwill92/service_2 ./Service_2
+docker build --no-cache -t coolwill92/service_2 ./Service_2
 docker push coolwill92/service_2
 
 
-docker build -t coolwill92/service_3 ./Service_3
+docker build --no-cache -t coolwill92/service_3 ./Service_3
 docker push coolwill92/service_3
 
 
-docker build -t coolwill92/service_4 ./Service_4
+docker build --no-cache -t coolwill92/service_4 ./Service_4
 docker push coolwill92/service_4
 
-docker build -t coolwill92/nginx ./NGINX
+docker build --no-cache -t coolwill92/nginx ./NGINX
 docker push coolwill92/nginx
--- scripts/update_swarm.sh
+++ scripts/update_swarm.sh
@@ -9,3 +9,5 @@ docker service update --image coolwill92/service_2:latest secondapp_service_2
 docker service update --image coolwill92/service_3:latest secondapp_service_3
 
 docker service update --image coolwill92/service_4:latest secondapp_service_4
+
+docker system prune -f
