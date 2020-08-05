d296a0d3327c1a992722929f4ed26f8690bff07a
coolwill92 <willpearce101@gmail.com>
2020-08-05 13:17:50 +0000

    Testin

--- scripts/build_swarm.sh
+++ scripts/build_swarm.sh
@@ -2,7 +2,7 @@
 
 #Takes running services and builds swarm! 
 
-source /var/lib/jenkins/.env
+source /home/William/Second_App.bashrc
 
 if [[ "$(docker stack services secondapp 2> /dev/null)" == "" ]]; then
     docker stack deploy --compose-file docker-compose.yml secondapp
