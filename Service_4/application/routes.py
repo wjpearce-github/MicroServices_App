7df5223279db4ff6a56f70ccffdfd43cd5469c43
coolwill92 <willpearce101@gmail.com>
2020-08-05 14:49:32 +0000

    tsaf

--- Service_4/application/routes.py
+++ Service_4/application/routes.py
@@ -4,7 +4,7 @@ import requests
 
 @app.route('/rpgname', methods=['GET'])
 def sentence():
-    beginning = requests.get('http://Service_2:5001/name')
-    ending = requests.get('http://Service_3:5002/name')
+    beginning = requests.get('http://service_2:5001/name')
+    ending = requests.get('http://service_3:5002/name')
     response = beginning.text + " " + ending.text
     return response
