2f3e3b10481dd9019bcb20786356c4562244ee26
coolwill92 <willpearce101@gmail.com>
2020-08-05 17:03:43 +0000

    jenkins test

--- Service_1/application/routes.py
+++ Service_1/application/routes.py
@@ -5,54 +5,38 @@ from os import environ
 import requests
 import random
 
+app.config['SECRET_KEY'] = '919c92fab903330df5b2f66c22d3b22b'  # environ.get('SECRET_KEY')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://' + \
+                                        environ.get('MYSQL_USER') + \
+                                        ':' + \
+                                        environ.get('MYSQL_PASSWORD') + \
+                                        '@' + \
+                                        environ.get('MYSQL_HOST') + \
+                                        ':' + \
+                                        environ.get('MYSQL_PORT') + \
+                                        '/' + \
+                                        environ.get('MYSQL_DB_NAME')
 
-# app.config['SECRET_KEY'] = environ.get('SECRET_KEY')
-# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://' + \
-#                                         environ.get('MYSQL_USER') + \
-#                                         ':' + \
-#                                         environ.get('MYSQL_PASSWORD') + \
-#                                         '@' + \
-#                                         environ.get('MYSQL_HOST') + \
-#                                         ':' + \
-#                                         environ.get('MYSQL_PORT') + \
-#                                         '/' + \
-#                                         environ.get('MYSQL_DB_NAME')
+db = SQLAlchemy(app)
 
-# db = SQLAlchemy(app)
-
-# class games(db.Model):
-#     id = db.Column(db.Integer, primary_key=True, unique=True)
-#     f_name = db.Column(db.String(50), nullable=False)
-
-
+class games(db.Model):
+    id = db.Column(db.Integer, primary_key=True, unique=True)
+    f_name = db.Column(db.String(50), nullable=False)
    
-#     def __repr__(self):
-#         return ''.join(
-#         [
-#             'First name ' + self.f_name + '\n'  
-#             'ID: ' + str(self.id)
-#         ]
-#     )
+    def __repr__(self):
+        return ''.join(
+        [
+            'F_name: ' + self.f_name + '\n' 
+            'ID: ' + str(self.id)
+        ]
+    )
 
 
-# @app.route('/', methods=['GET'])
-# def home():
-#     response = requests.get('http://service_4:5003/randomword')
-#     print(response)
-#     game = response.text
-#     game_data = games(
-#         f_name=game
-        
-#     )
-#     print(game)
-#     games_data = games.query.order_by(games.id.desc())
-#     db.session.add(game_data)
-#     db.session.commit()
-#     return render_template('index.html', game = game, games_data = games_data, title = 'Home')
-
-@app.route('/', methods=['GET'])
+@app.route('/', methods=['GET', 'POST'])
 def home():
-    response = requests.get('http://service_4:5003/rpgname')
-    rpgname = response.text
-    return render_template('index.html', rpgname = rpgname, title = 'Home')
+    response = requests.get('http://service_4:5003/merge')
+    print(response)
+    sentence = response.text
+    game_data = games.query.all()
+    return render_template('index.html', sentence=sentence, games=game_data, title='Home')
