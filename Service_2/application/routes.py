8dcc6f46ca7a9c98d2b69117ce31cbfd04d0fec1
coolwill92 <willpearce101@gmail.com>
2020-08-10 11:24:09 +0000

    Final Test

--- Service_2/application/routes.py
+++ Service_2/application/routes.py
@@ -2,9 +2,16 @@ from application import app
 import random
 
 
+# @app.route('/name', methods=['GET'])
+# def beginning():
+
+# 	list = ['Link','Fast','Tall','Bowser','Small','Big','Wizard', 'Angry']
+	
+# 	return list[random.randrange(8)]
+
 @app.route('/name', methods=['GET'])
 def beginning():
 
-	list = ['Link','Fast','Tall','Bowser','Small','Big','Wizard', 'Angry']
+	list = ['Kirby','Rapid','Luigui','Mario','Tiny','Fire','John', 'Samus']
 	
 	return list[random.randrange(8)]
