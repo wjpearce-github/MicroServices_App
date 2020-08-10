98d61e038841fcf160ad5ef80bda6d75486568e5
coolwill92 <willpearce101@gmail.com>
2020-08-10 11:30:45 +0000

    Final Test

--- Service_4/application/routes.py
+++ Service_4/application/routes.py
@@ -7,10 +7,16 @@ import random
 
 
 
+# @app.route('/randomword', methods=['GET'])
+# def sentence():
+#     beginning = requests.get('http://service_2:5001/name')
+#     ending = requests.get('http://service_3:5002/name')
+#     response = beginning.text + "" + ending.text
+#     return response
+
 @app.route('/randomword', methods=['GET'])
 def sentence():
     beginning = requests.get('http://service_2:5001/name')
     ending = requests.get('http://service_3:5002/name')
-    response = beginning.text + "" + ending.text
+    response = "The name is" beginning.text + "" + ending.text
     return response
-
