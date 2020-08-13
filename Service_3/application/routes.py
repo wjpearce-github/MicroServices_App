2457928f56ec95c527b52c7a159fd3300a8959b7
coolwill92 <willpearce101@gmail.com>
2020-08-13 10:07:59 +0000

    Example Change

--- Service_3/application/routes.py
+++ Service_3/application/routes.py
@@ -5,7 +5,7 @@ import random
 @app.route('/name', methods=['GET'])
 def ending():
 
-	list = ['Kong','Mac','Smith','Killer','Chomper','Biter','Samus', 'Jin']
-	# list = ['Kirby','Will','Heavy','King','Stinker','Tanky','Fire', 'Small']
+	# list = ['Kong','Mac','Smith','Killer','Chomper','Biter','Samus', 'Jin']
+	list = ['Kirby','Will','Heavy','King','Stinker','Tanky','Fire', 'Small']
 	
 	return list[random.randrange(8)]
